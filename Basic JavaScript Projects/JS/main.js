3e7bb7cb7aa6aee6c5bf473a7dda1b52f3210eda
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-07-26 17:10:32 -0600

    Changes

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -1,2 +1,7 @@
-var a = "Hello World!"
+var a = "Hello World!";
 document.write(a);
+window.alert("This is a window alert!");
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+document.write(Sent1+Sent2); //I think I wrote an expression here, I really hope so :)
