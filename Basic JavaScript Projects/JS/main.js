bddbc167e489f7b8113ba4467bffc3c4801b83fc
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-07-26 18:41:12 -0600

    Changes

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -3,5 +3,5 @@ document.write(a);
 window.alert("This is a window alert!");
 
 var Sent1 = "This is the beginning of the string";
-var Sent2 = " and this is the end of the string";
-document.write(Sent1+Sent2); //I think I wrote an expression here, I really hope so :)
+Sent1 += " and this is the end of the string";
+document.write(Sent1); //I think I wrote an expression here, I really hope so :)
---
+++ Project2_functions/js/main.js
@@ -0,0 +1,8 @@
+function changeColor(newColor) {
+  const elem = document.getElementById('hmw');
+  elem.style.color = newColor;
+}
+
+var a = "Thank you for";
+a += " using my website!";
+document.write(a); //Not gonna lie, I still kinda have now idea what I'm doing. Hopefully I figure it out soon :)
